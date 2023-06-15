088b5588766237b5e1546d90eecd20c8d99023dc
Lilla-Kavecsanszki <>
2023-06-15 17:58:16 +0000

    Deployment commit

--- yoga_studio/settings.py
+++ yoga_studio/settings.py
@@ -29,7 +29,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['8000-lilla-kavecsanszki-downw-he3rojkpv5.us2.codeanyapp.com',
                  'downwarddog.herokuapp.com', 'localhost']
