b1d89c172ae53e31a8ffc037bc2f882d5b66bd2d
Lilla-Kavecsanszki <>
2023-07-10 10:44:06 +0000

    Add form for the Booking model

--- downwarddog/forms.py
+++ downwarddog/forms.py
@@ -6,3 +6,9 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
+
+
+class BookingForm(forms.ModelForm):
+    class Meta:
+        model = Booking
+        fields = ['booking_date', 'start_time', 'class_type', 'description']
